ebbdf367c9ad30193f38b99eef075eb33a90d7ee
Jvol <damnedprotoss@gmail.com>
2019-04-23 02:28:10 +0300

    learning stuff

---
+++ main.py
@@ -0,0 +1,44 @@
+import cv2
+import sys
+
+cascPath = sys.argv[1]
+eyecaspath = sys.argv[2]
+faceCascade = cv2.CascadeClassifier(cascPath)
+eye_cascade = cv2.CascadeClassifier(eyecaspath)
+video_capture = cv2.VideoCapture(0)
+i = 0
+
+while True:
+    # Capture frame-by-frame
+    ret, frame = video_capture.read()
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    i = i + 1
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=2.1,
+        minNeighbors=5,
+        minSize=(30, 30),
+        flags=cv2.CASCADE_SCALE_IMAGE
+    )
+
+    # Draw a rectangle around the faces
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), 2)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = frame[y:y+h, x:x+w]
+        eyes = eye_cascade.detectMultiScale(roi_gray,scaleFactor=4.2)
+        for (ex,ey,ew,eh) in eyes:
+            cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+            print("dalyarak" + str(i) )
+
+    # Display the resulting frame
+    cv2.imshow('Video', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything is done, release the capture
+video_capture.release()
+cv2.destroyAllWindows()
+

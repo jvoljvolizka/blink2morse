a314db464776112a35d0539f8e33df4009ce2733
Jvol <damnedprotoss@gmail.com>
2019-04-23 16:14:28 +0300

    slowly

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import cv2
 import sys
+#import pygame
+#i'm using pygame beacause he is too cute but nobody likes him. I feel sad about that cute python library
 
 cascPath = sys.argv[1]
 eyecaspath = sys.argv[2]
@@ -7,7 +9,13 @@ faceCascade = cv2.CascadeClassifier(cascPath)
 eye_cascade = cv2.CascadeClassifier(eyecaspath)
 video_capture = cv2.VideoCapture(0)
 i = 0
-
+frameWidth = int(video_capture.get(cv2.CAP_PROP_FRAME_WIDTH))
+frameHeight = int(video_capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
+lockeyes = False
+#(width, height) = (1000, 1000)
+#screen = pygame.display.set_mode((width, height))
+#pygame.display.flip()
+#pygame.draw.line(screen, (255,0,255), (20,20), (70,80), 2)
 while True:
     # Capture frame-by-frame
     ret, frame = video_capture.read()
@@ -21,7 +29,6 @@ while True:
         minSize=(30, 30),
         flags=cv2.CASCADE_SCALE_IMAGE
     )
-
     # Draw a rectangle around the faces
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), 2)
@@ -29,10 +36,16 @@ while True:
         roi_color = frame[y:y+h, x:x+w]
         eyes = eye_cascade.detectMultiScale(roi_gray,scaleFactor=4.2)
         for (ex,ey,ew,eh) in eyes:
+            lockeyes = True
             cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
-            print("dalyarak" + str(i) )
+            print(eyes.shape[0])
 
-    # Display the resulting frame
+    if(lockeyes == True):
+                cv2.putText(img = frame, text = "i can see your dark soul from these eyes and it's delicious",org = (0,50), fontFace = cv2.FONT_HERSHEY_DUPLEX, fontScale = 1,color = (0, 0, 0))
+
+
+
+# Display the resulting frame
     cv2.imshow('Video', frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
